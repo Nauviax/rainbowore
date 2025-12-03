bf3d2c66740a9d57706f5eb5c3d795c5c56b061f
Nauviax <nauviax@icloud.com>
2025-12-04 12:01:57 +1300

    Made black tints lighter so they show

--- data.lua
+++ data.lua
@@ -7,7 +7,7 @@ vanillaRawResouceData["uranium-ore"] = {
     rainbow_amount_required = 10, -- Old was 5 -> 5, new is 1/2 as good
     energy_required = 5,
     result_count = 5,
-    additional_ingredients = {{name="sulfuric-acid",amount=5,type="fluid"}},
+    additional_ingredients = {{ name = "sulfuric-acid", amount = 5, type = "fluid"}},
     order = "a[conversion]-c[uranium-ore]", -- Last / Sixth
     color = {0.4, 0.7, 0.0}
 }
@@ -16,9 +16,9 @@ vanillaRawResouceData["crude-oil"] = {
     rainbow_amount_required = 5, -- Old was 25 -> 250, new is 4x better
     energy_required = 5,
     result_count = 200,
-    additional_ingredients = {{name="water",amount=200,type="fluid"}},
+    additional_ingredients = {{ name = "water", amount = 200, type = "fluid" }},
     order = "a[conversion]-b[crude-oil]", -- Second-to-last / Fifth
-    color = {0.0, 0.0, 0.1}
+    color = {0.4, 0.4, 0.5}
 }
 vanillaRawResouceData["iron-ore"] = {
     order = "a[conversion]-a[earlyores]-a[iron-ore]", -- First
@@ -34,7 +34,7 @@ vanillaRawResouceData["stone"] = {
 }
 vanillaRawResouceData["coal"] = {
     order = "a[conversion]-a[earlyores]-d[coal]", -- Fourth
-    color = {0.0, 0.0, 0.0}
+    color = {0.4, 0.4, 0.4}
 }
 
 -- Use preset rainboworedata customization on the resource
